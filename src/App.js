eb07a46373b873ee18d6b7b86f70dd4cc18e4ed8
동동이 <drdragon777@naver.com>
2021-06-27 13:37:41 +0900

    [ADD] (#3) - add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import QrGenerator from './components/QrGenerator'
+import QrScanner from './components/QrScanner'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/QrGen" component={QrGenerator} />
+        <Route exact path="/QrScan" component={QrScanner} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
