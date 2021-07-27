bbdf94fc12fc3a124f3bd6ecf433ff014dbfc864
동동이 <drdragon777@naver.com>
2021-07-27 15:52:55 +0900

    [ADD] (#4, #5) - add webpage structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,109 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { BrowserRouter, Route, Switch, Link, Redirect } from 'react-router-dom'
+import { withCookies, useCookies } from 'react-cookie'
+import PropTypes from 'prop-types'
+
 import QrGenerator from './components/QrGenerator'
 import QrScanner from './components/QrScanner'
-import Login from './components/Login'
+import Login from './components/Login/login'
 import RegisterForm from './components/Register'
+import Home from './components/content/home'
+import Profile from './components/profile'
+import Courses from './components/course'
+
+import { logout } from './actions/auth'
+import { clearMessage } from './actions/message'
+import { history } from './helpers/history'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [cookies, removeCookie, setCookie, getCookie] = useCookies(['user'])
+  const [hasCookie, setHasCookie] = useState(false)
+
+  // const { user: currentUser } = useSelector(state => state.auth)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    history.listen(location => dispatch(clearMessage()))
+  }, [dispatch])
+
+  const logOut = () => {
+    dispatch(logout())
+  }
+
+  useEffect(() => {
+    if (cookies.user && cookies.user !== 'undefined') {
+      setHasCookie(true)
+    } else {
+      setHasCookie(false)
+    }
+  }, [cookies])
+
   return (
-    <BrowserRouter>
+    <BrowserRouter history={history}>
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to="/" className="navbar-brand">
+            히어(Here)
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link">
+                Home
+              </Link>
+            </li>
+          </div>
+          {/* {hasCookie ? ( */}
+          <div className="navbar-nav ml-auto">
+            {/* <li className="nav-item">
+                <Link to="/profile" className="nav-link">
+                  {cookies.user.userName}
+                </Link>
+              </li> */}
+            <li className="nav-item">
+              <Link to="/QrGen" className="nav-link">
+                출석하기
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Course" className="nav-link">
+                강좌보기
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a href="/" className="nav-link" onClick={logOut}>
+                로그아웃
+              </a>
+            </li>
+          </div>
+          {/* ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  로그인
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/register" className="nav-link">
+                  회원가입
+                </Link>
+              </li>
+            </div>
+          )} */}
+        </nav>
+      </div>
+      {/* {!hasCookie ? <Redirect to="/login" /> : <Redirect to="QrScan" />} */}
       <Switch>
+        <Route exact path={['/', '/home']} component={Home} />
         <Route exact path="/Login" component={Login} />
         <Route exact path="/Register" component={RegisterForm} />
+        <Route exact path="/profile" component={Profile} />
         <Route exact path="/QrGen" component={QrGenerator} />
         <Route exact path="/QrScan" component={QrScanner} />
+        <Route exact path="/Course" component={Courses} />
       </Switch>
     </BrowserRouter>
   )
