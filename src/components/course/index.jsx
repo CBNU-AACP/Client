cbf7baaf58183cc8f072d20e5a3dc3eea627bffe
동동이 <drdragon777@naver.com>
2021-07-31 15:40:30 +0900

    [ADD] (#7) - add course cookie auth

--- src/components/course/index.jsx
+++ src/components/course/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, Redirect } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { withCookies, useCookies } from 'react-cookie'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import AddCourse from './CreateCourse'
@@ -8,11 +9,21 @@ import Course from './CoursesList/course'
 import CoursesList from './CoursesList'
 
 function Courses() {
-  const { user: currentUser } = useSelector(state => state.auth)
+  // const { user: currentUser } = useSelector(state => state.auth)
   const { path, url } = useRouteMatch()
+  const [cookies, removeCookie, setCookie, getCookie] = useCookies(['user'])
+  const [hasCookie, setHasCookie] = useState(false)
+
+  useEffect(() => {
+    if (cookies.user && cookies.user !== 'undefined') {
+      setHasCookie(true)
+    } else {
+      setHasCookie(false)
+    }
+  }, [cookies])
   return (
     <Router>
-      {currentUser ? (
+      {/* {hasCookie ? ( */}
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
@@ -27,9 +38,9 @@ function Courses() {
             </li>
           </div>
         </nav>
-      ) : (
+      {/* ) : (
         <Redirect to="/" />
-      )}
+      )} */}
 
       <div className="container mt-3">
         <Switch>
