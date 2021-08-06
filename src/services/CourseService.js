523ab9ab94a995a23afec565e159c7cec718502c
동동이 <drdragon777@naver.com>
2021-08-06 23:59:27 +0900

    [FIX] (#7) - modify course redux

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,18 +1,18 @@
-import http from '../http'
+import http from '../common/http'
 
-const getAll = () => http.get('v1/courses/multiple/test12')
+const getAll = () => http.get('v1/courses/multiple/test1')
 
 const get = id => http.get(`v1/courses/single/${id}`)
 
-const create = data => http.post('v1/courses/test12', data)
+const create = data => http.post('v1/courses/test1', data)
 
 const update = (id, data) => http.put(`v1/courses/${id}`, data)
 
 const remove = id => http.delete(`v1/courses/single/${id}`)
 
-const removeAll = () => http.delete(`v1/courses/multiple/test12`)
+const removeAll = () => http.delete(`v1/courses/multiple/test1`)
 
-const findByName = name => http.get(`v1/courses/search/test12?value=${encodeURIComponent(name)}`)
+const findByName = name => http.get(`v1/courses/search/test1?value=${encodeURIComponent(name)}`)
 
 const CourseService = {
   getAll,
