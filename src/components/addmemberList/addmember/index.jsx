a864e9ae22ca1c49f5b3cdc8e53871d39512c7c0
동동이 <drdragon777@naver.com>
2021-08-13 17:40:11 +0900

    [RFR] (#8) - delete update method for member component

--- src/components/addmemberList/addmember/index.jsx
+++ src/components/addmemberList/addmember/index.jsx
@@ -5,64 +5,29 @@ import React, { useState, useEffect, useCallback } from 'react'
 import StyledMember from './style'
 import PropTypes from 'prop-types'
 
-const Member = ({ member, updateMember, removeMember }) => {
+const Member = ({ member, removeMember }) => {
   Member.propTypes = {
     member: PropTypes.func.isRequired,
-    updateMember: PropTypes.func.isRequired,
     removeMember: PropTypes.func.isRequired,
   }
 
-  const [userId, setUserId] = useState(member.userId)
-  const [editMode, setEditMode] = useState(true)
-
-  const handleChange = e => {
-    setUserId(e.target.value)
-  }
-
-  const handleSubmit = e => {
-    updateMember({
-      ...member,
-      userId,
-    })
-    setEditMode(false)
-  }
+  const userId = member.userId
 
   const handleMinus = () => {
     removeMember(member.id)
   }
 
-  const activeEditMode = () => {
-    setEditMode(true)
-  }
-
   return (
     <StyledMember>
-      {editMode ? (
-        <div className="add">
-          <input
-            type="text"
-            onChange={handleChange}
-            name="userId"
-            className="id"
-            value={userId}
-            placeholder="학생의 아이디"
-          />
-          <button type="button">
-            <FiCheckSquare className="check" onClick={handleSubmit}></FiCheckSquare>
-            <FiMinusSquare className="minus" onClick={handleMinus}></FiMinusSquare>
-          </button>
-        </div>
-      ) : (
-        <div className="add">
-          <p id="userId" className="submitted_id">
-            {userId}
-          </p>
-          <button type="button">
-            <BsPencilSquare className="modify" onClick={activeEditMode}></BsPencilSquare>
-            <FiMinusSquare className="minus" onClick={handleMinus}></FiMinusSquare>
-          </button>
-        </div>
-      )}
+      <div className="add">
+        <p id="userId" className="submitted_id">
+          {userId}
+        </p>
+        <button type="button">
+          <FiMinusSquare className="minus" onClick={handleMinus}></FiMinusSquare>
+        </button>
+      </div>
+      )
     </StyledMember>
   )
 }
