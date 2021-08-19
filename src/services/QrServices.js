6d035171053faa1f656e8b02aafa6500cf485529
동동이 <drdragon777@naver.com>
2021-08-19 21:38:53 +0900

    [ADD] (#12) - add qrservice router

---
+++ src/services/QrServices.js
@@ -0,0 +1,15 @@
+import http from '../common/http'
+
+const put = validNum => http.put(`v1/users/test1`, { validNum })
+
+const get = courseId => http.get(`v1/courseDates/${courseId}`)
+
+const patch = info => http.patch(`v1/attendance/attend`, info)
+
+const QrService = {
+  put,
+  get,
+  patch,
+}
+
+export default QrService
