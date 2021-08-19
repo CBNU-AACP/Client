77ae77d9394e0eb77373f9e883dde4a4c98de3e1
JuYeong <happyu9986@gmail.com>
2021-08-20 01:13:20 +0900

    [ADD] (#5) - add register form userPhoneNum, VerifyNum item

--- src/components/Register/yup.jsx
+++ src/components/Register/yup.jsx
@@ -11,13 +11,13 @@ export const schema = yup.object({
     .required('이름을 입력해주세요.')
     .max(15, '이름은 15자리 이하여야 합니다.')
     .min(2, '이름은 2자리 이상이어야 합니다.'),
-  memberId: yup.string().required('학번을 입력해주세요. 학번이 없으시면 "null"을 입력해주세요'),
+  studentId: yup.number().required('학번을 입력해주세요. 학번이 없으시면 "null"을 입력해주세요.').typeError('학번을 올바르게 입력해주세요.'),
   email: yup.string().required('이메일을 입력해주세요.').email('잘못된 형식의 이메일입니다.'),
   passWord: yup
     .string()
     .required('비밀번호를 입력해주세요.')
-    .max(15, '비밀번호는 10자리 이하여야 합니다.')
-    .min(4, '비밀번호는 6자리 이상이어야 합니다.'),
+    .max(20, '비밀번호는 20자리 이하여야 합니다.')
+    .min(6, '비밀번호는 6자리 이상이어야 합니다.'),
   passWord2: yup
     .string()
     .required('비밀번호를 확인해주세요.')
