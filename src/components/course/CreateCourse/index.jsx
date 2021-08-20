14569e58d3935ea7db428e187249e0bff7ae6e90
동동이 <drdragon777@naver.com>
2021-08-20 22:46:01 +0900

    [ADD] (#16) - add createcourse design

--- src/components/course/CreateCourse/index.jsx
+++ src/components/course/CreateCourse/index.jsx
@@ -2,6 +2,10 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { createCourse } from '../../../actions/courses'
+import StyledCrCourse from './style'
+
+import { GoBook, GoKeyboard } from 'react-icons/go'
+import { AiFillCheckCircle } from 'react-icons/ai'
 
 const AddCourse = () => {
   const initialCourseState = {
@@ -43,48 +47,64 @@ const AddCourse = () => {
   }
 
   return (
-    <div className="submit-form">
-      {submitted ? (
-        <div>
-          <h4>성공적으로 등록되었습니다!</h4>
-          <button type="submit" className="btn btn-success" onClick={newCourse}>
-            추가등록
-          </button>
-        </div>
-      ) : (
-        <div>
-          <div className="form-group">
-            <label htmlFor="name">강좌명</label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              required
-              value={course.name}
-              onChange={handleInputChange}
-              name="name"
-            />
+    <StyledCrCourse>
+      <div className="container">
+        {submitted ? (
+          <div>
+            <h4>성공적으로 등록되었습니다!</h4>
+            <button type="submit" className="" onClick={newCourse}>
+              추가등록
+            </button>
           </div>
+        ) : (
+          <div className="formGroup">
+            <div className="form">
+              <div className="icons">
+                <GoBook className="icon" />
+                <label htmlFor="name" className="label">
+                  강좌이름
+                </label>
+              </div>
 
-          <div className="form-group">
-            <label htmlFor="description">설명</label>
-            <input
-              type="text"
-              className="form-control"
-              id="description"
-              required
-              value={course.description}
-              onChange={handleInputChange}
-              name="description"
-            />
+              <input
+                type="text"
+                className=""
+                id="name"
+                required
+                value={course.name}
+                placeholder="강좌이름을 입력해주세요."
+                onChange={handleInputChange}
+                name="name"
+              />
+            </div>
+
+            <div className="form">
+              <div className="icons">
+                <GoKeyboard className="icon" />
+                <label htmlFor="description" className="label">
+                  강좌설명
+                </label>
+              </div>
+              <input
+                type="text"
+                className=""
+                id="description"
+                required
+                value={course.description}
+                placeholder="강좌설명을 입력해주세요."
+                onChange={handleInputChange}
+                name="description"
+              />
+            </div>
+
+            <button type="submit" onClick={saveCourse} className="submit">
+              <AiFillCheckCircle className="icon" />
+              <p className="label">강좌등록</p>
+            </button>
           </div>
-
-          <button type="submit" onClick={saveCourse} className="btn btn-success">
-            등록
-          </button>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </StyledCrCourse>
   )
 }
 
--- src/components/course/CreateCourse/style.jsx
+++ src/components/course/CreateCourse/style.jsx
@@ -1,7 +1,59 @@
 import styled from 'styled-components'
 
 const CrCourse = styled.div`
-  margin-top: 20px;
+  margin: 8rem 0 0 0;
+  align-items: center;
+  max-width: 300px;
+  width: 100%;
+  height: 200px;
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  border-radius: 20px;
+  .container {
+    margin: auto;
+    .formGroup {
+      margin: 2rem auto;
+      width: max-content;
+      .form {
+        margin: 1rem;
+        .icons {
+          .icon {
+            font-size: 1.3rem;
+          }
+          .label {
+            margin: auto 0.2rem;
+            font-size: 0.8rem;
+          }
+        }
+        input {
+          font-size: 0.7rem;
+          border: 0;
+          border-bottom: 2px solid black;
+        }
+        input::placeholder {
+          font-size: 0.7rem;
+        }
+      }
+      .submit {
+        margin: 0 0 0 6.7rem;
+        display: flex;
+        background-color: #dcdcdc;
+        border-radius: 5px;
+        .icon {
+          font-size: 1.4rem;
+        }
+        .label {
+          margin: auto;
+          font-size: 0.8rem;
+        }
+      }
+      .submit:hover {
+        background-color: #c8c8c8;
+        .icon {
+          color: orange;
+        }
+      }
+    }
+  }
 `
 
 export default CrCourse
