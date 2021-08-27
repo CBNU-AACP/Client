debbbf3e66429f5e783612c2d9c36cbbbfbfc54d
JuYeong <happyu9986@gmail.com>
2021-08-28 01:06:33 +0900

    [RFT] (#11, #23) - login style 통일 및 회원가입 버튼 추가

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -2,14 +2,12 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Redirect } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import PropTypes from 'prop-types'
 import { Button } from 'antd'
 import StyedLogin from './style'
-
 import { login } from '../../actions/auth'
-
 import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
 import IconButton from '@material-ui/core/IconButton'
@@ -17,26 +15,25 @@ import Input from '@material-ui/core/Input'
 import InputLabel from '@material-ui/core/InputLabel'
 import InputAdornment from '@material-ui/core/InputAdornment'
 import FormControl from '@material-ui/core/FormControl'
-import TextField from '@material-ui/core/TextField'
 import Visibility from '@material-ui/icons/Visibility'
 import VisibilityOff from '@material-ui/icons/VisibilityOff'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  margin: {
-    margin: theme.spacing(1),
-  },
-  withoutLabel: {
-    marginTop: theme.spacing(3),
-  },
-  textField: {
-    width: '25ch',
-  },
-}))
-const Login = props => {
+function Login(props) {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    withoutLabel: {
+      marginTop: theme.spacing(3),
+    },
+    textField: {
+      width: '25ch',
+    },
+  }))
   Login.propTypes = {
     history: PropTypes.shape({
       push: PropTypes.func.isRequired,
@@ -54,21 +51,9 @@ const Login = props => {
   const { isLoggedIn } = useSelector(state => state.auth)
   const { message } = useSelector(state => state.message)
 
-  const dispatch = useDispatch()
-
   const handleLogin = data => {
     setLoading(true)
     console.log(data.userId, data.passWord)
-    // setUserId(data.userId)
-    // setPassword(data.passWord)
-    dispatch(login(data.userId, data.passWord))
-      .then(() => {
-        props.history.push('/profile')
-        window.location.reload()
-      })
-      .catch(() => {
-        setLoading(false)
-      })
   }
 
   if (isLoggedIn) {
@@ -76,11 +61,9 @@ const Login = props => {
   }
 
   const classes = useStyles()
-  const [values, setValues] = React.useState({
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
+  const [values, setValues] = useState({
+    userId: '',
+    userPassword: '',
     showPassword: false,
   })
 
@@ -101,13 +84,15 @@ const Login = props => {
       <div className={classes.root}>
         <div className="login">
           <div className="form-group">
-            <TextField
-              id="standard-start-adornment"
-              className={clsx(classes.margin, classes.textField)}
-              InputProps={{
-                startAdornment: <InputAdornment position="start">아이디</InputAdornment>,
-              }}
-            />
+            <FormControl className={clsx(classes.margin, classes.textField)}>
+              <InputLabel htmlFor="standard-adornment-userid">아이디</InputLabel>
+              <Input
+                id="standard-adornment-userid"
+                type="text"
+                value={values.userId}
+                onChange={handleChange('userId')}
+              />
+            </FormControl>
           </div>
           <div className="form-group">
             <FormControl className={clsx(classes.margin, classes.textField)}>
@@ -115,8 +100,8 @@ const Login = props => {
               <Input
                 id="standard-adornment-password"
                 type={values.showPassword ? 'text' : 'password'}
-                value={values.password}
-                onChange={handleChange('password')}
+                value={values.userPassword}
+                onChange={handleChange('userPassword')}
                 endAdornment={
                   <InputAdornment position="end">
                     <IconButton
@@ -135,6 +120,11 @@ const Login = props => {
               {loading && <span className="spinner-border spinner-border-sm"></span>}
               <span>Login</span>
             </Button>
+          </div>
+          <div>
+            <Link to="/register" className="iconList">
+              <p className="label">회원가입</p>
+            </Link>
           </div>
 
           {message && (
