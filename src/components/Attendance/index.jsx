6914661dc4a26f0dd9eac7c93c1a19b3b4d192bc
동동이 <drdragon777@naver.com>
2021-09-07 01:09:12 +0900

    [ADD] (#6) - attendance data grid 약간의 스타일 변경

--- src/components/Attendance/index.jsx
+++ src/components/Attendance/index.jsx
@@ -47,36 +47,36 @@ function Attendance(props) {
 
   const columns = [
     {
-      field: 'firstName',
-      headerName: 'First name',
-      width: 150,
+      field: '학번',
+      headerName: '학번',
+      width: 100,
       editable: true,
     },
     {
-      field: 'lastName',
-      headerName: 'Last name',
-      width: 150,
+      field: '이름',
+      headerName: '학번',
+      width: 100,
       editable: true,
     },
     {
-      field: 'age',
-      headerName: 'Age',
+      field: '2021090613',
+      headerName: '2021090613',
       type: 'number',
-      width: 110,
+      width: 200,
       editable: true,
     },
   ]
 
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, 이름: 'Snow', 학번: 'Jon', 2021090613: 35 },
+    { id: 2, 이름: 'Lannister', 학번: 'Cersei', 2021090613: 42 },
+    { id: 3, 이름: 'Lannister', 학번: 'Jaime', 2021090613: 45 },
+    { id: 4, 이름: 'Stark', 학번: 'Arya', 2021090613: 16 },
+    { id: 5, 이름: 'Targaryen', 학번: 'Daenerys', 2021090613: null },
+    { id: 6, 이름: 'Melisandre', 학번: null, 2021090613: 150 },
+    { id: 7, 이름: 'Clifford', 학번: 'Ferrara', 2021090613: 44 },
+    { id: 8, 이름: 'Frances', 학번: 'Rossini', 2021090613: 36 },
+    { id: 9, 이름: 'Roxie', 학번: 'Harvey', 2021090613: 65 },
   ]
 
   return (
@@ -89,8 +89,17 @@ function Attendance(props) {
             <p>총 학생수: 35명</p>
           </div>
 
-          <div className="datagrid" style={{ height: 400, width: '600px' }}>
-            <DataGrid rows={rows} columns={columns} pageSize={10} rowsPerPageOptions={[10]} disableSelectionOnClick />
+          <div className="datagrid">
+            <div className="gridparent">
+              <DataGrid
+                autoHeight
+                rows={rows}
+                columns={columns}
+                pageSize={10}
+                rowsPerPageOptions={[10]}
+                disableSelectionOnClick
+              />
+            </div>
           </div>
         </div>
       ) : (
