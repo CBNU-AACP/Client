a107572cf1ac2113fd5be153f72c29356bfcbb05
JuYeong <happyu9986@gmail.com>
2021-09-08 00:59:01 +0900

    [CHO] - cookie파일 따로 - 추후구현

---
+++ src/components/Login/cookies.jsx
@@ -0,0 +1,13 @@
+// import { Cookies } from 'react-cookie'
+// const cookies = new Cookies()
+
+// export const setCookie = (name, value, option) => {
+//   console.log(name, value, option)
+//   return cookies.set(name, value, { ...option })
+// }
+
+// export const getCookie = name => {
+//   return cookies.get(name)
+// }
+
+// userId: string, userPassword: string
