35b06a7ec3f2e63d046a7ffaa57cf80ed6d8a4c1
동동이 <drdragon777@naver.com>
2021-09-08 00:14:09 +0900

    [ADD] (#39) - add qrgen component cookie props

--- src/App.js
+++ src/App.js
@@ -50,13 +50,13 @@ function App() {
       <BrowserRouter history={history}>
         {/* {!hasCookie ? <Redirect to="/login" /> : <Redirect to="QrScan" />} */}
         <Switch>
-          <Route exact path={['/', '/home']} component={Home} />
+          <Route exact path={['/', '/home']} render={() => <Home cookies={cookies} />} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/find" component={Find} />
           <Route exact path="/register" component={Register} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/qrgen" component={QrGenerator} />
-          <Route exact path="/courses" component={Courses} />
+          <Route exact path="/profile" render={() => <Profile cookies={cookies} />} />
+          <Route exact path="/qrgen" render={() => <QrGenerator cookies={cookies} />} />
+          <Route exact path="/courses" render={() => <Courses cookies={cookies} />} />
         </Switch>
         {hasCookie ? (
           <div>
