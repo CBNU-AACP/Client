d19bb9bc72dce4ee8fcdc607379bf6edd17ccb21
JuYeong <happyu9986@gmail.com>
2021-09-09 15:09:31 +0900

    [RFT](#11)-login redux

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -45,14 +45,10 @@ export const LoginUser = user => async dispatch => {
     console.log(user)
     const res = await AuthService.login(user)
     console.log(res)
-    // dispatch({
-    //   type: LOGIN_SUCCESS,
-    //   payload: res.data.data,
-    // })
-    // dispatch({
-    //   type: SET_MESSAGE,
-    //   payload: res.data.message,
-    // })
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data.data,
+    })
 
     return Promise.resolve(res.data.data)
   } catch (err) {
@@ -69,11 +65,22 @@ export const logout = () => dispatch => {
   })
 }
 
-// 전화번호 인증
-export const PhoneVerify = (userId, userPhoneNumber) => async dispatch => {
+// 전화번호 인증번호 받기
+export const GetPhoneVerifyNum = (userId, userPhoneNumber) => async dispatch => {
   try {
     console.log(userId, userPhoneNumber)
-    const res = await AuthService.phoneverify(userId, userPhoneNumber)
+    const res = await AuthService.GetPhoneVerifyNum(userId, userPhoneNumber)
+    return Promise.resolve(res.data.data)
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
+
+// 전화번호 인증번호 입력
+export const PhoneVerify = (userId, Verifykey) => async dispatch => {
+  try {
+    console.log(userId, Verifykey)
+    const res = await AuthService.GetPhoneVerifyNum(userId, Verifykey)
     return Promise.resolve(res.data.data)
   } catch (err) {
     return Promise.reject(err)
