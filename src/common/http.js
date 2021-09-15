211a8ab230ea9b332522540cc71b8fb324c35eee
JuYeong <happyu9986@gmail.com>
2021-09-15 17:58:15 +0900

    [DEL]-http삭제 server url정보 노출방지

--- src/common/http.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: 'https://peaceful-basin-80472.herokuapp.com/',
-  headers: {
-    'Content-type': 'application/json',
-  },
-})
