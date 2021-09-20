4e1f1cd4f938d65629734ac93c551d6cdc626dba
JuYeong <happyu9986@gmail.com>
2021-09-21 02:45:15 +0900

    [RFR]-login컴포넌트에서 사용할 메인파일 path 변경

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,45 +1,46 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Redirect } from 'react-router-dom'
-import { useForm } from 'react-hook-form'
+import { Link, Redirect } from 'react-router-dom'
+import { Controller, useForm } from 'react-hook-form'
 import PropTypes from 'prop-types'
 import { Button } from 'antd'
-
-import { login } from '../../actions/auth'
-
+import StyedLogin from './style'
+// import { setCookie, getCookie } from './cookies'
+import { useCookies } from 'react-cookie'
+import { LoginUser } from '../../actions/auth'
 import clsx from 'clsx'
 import { makeStyles } from '@material-ui/core/styles'
 import IconButton from '@material-ui/core/IconButton'
 import Input from '@material-ui/core/Input'
-import FilledInput from '@material-ui/core/FilledInput'
-import OutlinedInput from '@material-ui/core/OutlinedInput'
 import InputLabel from '@material-ui/core/InputLabel'
 import InputAdornment from '@material-ui/core/InputAdornment'
-import FormHelperText from '@material-ui/core/FormHelperText'
 import FormControl from '@material-ui/core/FormControl'
-import TextField from '@material-ui/core/TextField'
 import Visibility from '@material-ui/icons/Visibility'
 import VisibilityOff from '@material-ui/icons/VisibilityOff'
-// import { Button } from 'bootstrap'
+import { Typography } from '@material-ui/core'
+import LockIcon from '@material-ui/icons/Lock'
+import PersonIcon from '@material-ui/icons/Person'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  margin: {
-    margin: theme.spacing(1),
-  },
-  withoutLabel: {
-    marginTop: theme.spacing(3),
-  },
-  textField: {
-    width: '25ch',
-  },
-}))
-const Login = props => {
+function Login(props, { match }) {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    withoutLabel: {
+      marginTop: theme.spacing(3),
+    },
+    textField: {
+      width: '25ch',
+    },
+  }))
+  const dispatch = useDispatch()
+
   Login.propTypes = {
     history: PropTypes.shape({
       push: PropTypes.func.isRequired,
@@ -49,57 +50,14 @@ const Login = props => {
     register,
     handleSubmit,
     setValue,
+    control,
     formState: { errors },
   } = useForm()
 
-  //   const form = useRef()
-  //   const checkBtn = useRef()
-
-  //   const [userId, setUserId] = useState('')
-  //   const [passWord, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
-
-  const { isLoggedIn } = useSelector(state => state.auth)
-  const { message } = useSelector(state => state.message)
-
-  const dispatch = useDispatch()
-
-  //   const onChangeUserid = e => {
-  //     const [name, value] = e.target
-  //     console.log(name, value)
-  //     setUserId(value)
-  //   }
-
-  //   const onChangePassword = e => {
-  //     const password = e.target.value
-  //     setPassword(password)
-  //   }
-
-  const handleLogin = data => {
-    setLoading(true)
-    console.log(data.userId, data.passWord)
-    // setUserId(data.userId)
-    // setPassword(data.passWord)
-    dispatch(login(data.userId, data.passWord))
-      .then(() => {
-        props.history.push('/profile')
-        window.location.reload()
-      })
-      .catch(() => {
-        setLoading(false)
-      })
-  }
-
-  if (isLoggedIn) {
-    return <Redirect to="/profile" />
-  }
-
   const classes = useStyles()
-  const [values, setValues] = React.useState({
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
+  const [values, setValues] = useState({
+    userId: '',
+    userPassword: '',
     showPassword: false,
   })
 
@@ -114,86 +72,111 @@ const Login = props => {
   const handleMouseDownPassword = event => {
     event.preventDefault()
   }
+  const [cookies, setCookie, removeCookie] = useCookies(['userId'])
+  const [cookieExpires] = useState({
+    now: new Date(),
+    after1h: new Date(),
+  })
+
+  const handleLogin = data => {
+    console.log('submit', data)
+    const { userId, userPassword } = data
+    const user = { userId, userPassword }
+    cookieExpires.after1h.setHours(cookieExpires.now.getHours() + 1)
+    console.log(cookieExpires.after1h)
+    dispatch(LoginUser(user))
+      .then(e => {
+        console.log(e)
+        setCookie('userId', user.userId, { path: '/', expires: cookieExpires.after1h })
+      })
+      .catch(e => {
+        console.log('error', e)
+        removeCookie('userId')
+        alert('아이디 또는 비밀번호를 다시 확인해주세요.')
+      })
+  }
 
   return (
-    <div className={classes.root}>
-      <div className="card card-container">
-        <form onSubmit={handleSubmit(handleLogin)}>
-          <div className="form-group">
-            <label htmlFor="userId">아이디</label>
-            <input
-              type="text"
-              className="form-control"
-              id="userId"
-              name="userId"
-              placeholder="아이디"
-              onChange={e => setValue('userId', e.target.value)}
-              //   onChange={onChangeUserid}
-              {...register('userId', { required: true })}
-            />
-            {errors.userId && <span>아이디를 입력해주세요.</span>}
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="passWord">비밀번호</label>
-            <input
-              type="password"
-              className="form-control"
-              id="passWord"
-              name="passWord"
-              placeholder="비밀번호"
-              //   onChange={onChangePassword}
-              {...register('passWord', { required: true })}
-            />
-            {errors.passWord && <span>비밀번호를 입력해주세요.</span>}
-          </div>
-          <div>
-            <TextField
-              label="With normal TextField"
-              id="standard-start-adornment"
-              className={clsx(classes.margin, classes.textField)}
-              InputProps={{
-                startAdornment: <InputAdornment position="start">아이디</InputAdornment>,
-              }}
-            />
-            <FormControl className={clsx(classes.margin, classes.textField)}>
-              <InputLabel htmlFor="standard-adornment-password">비밀번호</InputLabel>
-              <Input
-                id="standard-adornment-password"
-                type={values.showPassword ? 'text' : 'password'}
-                value={values.password}
-                onChange={handleChange('password')}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}>
-                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-          </div>
-
-          <div className="form-group">
-            <Button type="submit" value="Login" className="btn btn-primary btn-block" disabled={loading}>
-              {loading && <span className="spinner-border spinner-border-sm"></span>}
-              <span>Login</span>
-            </Button>
-          </div>
-
-          {message && (
-            <div className="form-group">
-              <div className="alert alert-danger" role="alert">
-                {message}
-              </div>
+    <StyedLogin onFinish={handleSubmit(handleLogin)} size="large">
+      {!cookies.userId || cookies.userId === 'undefined' ? (
+        <div className={classes.root}>
+          <div className="box">
+            <Typography variant="h5" align="center">
+              로그인
+            </Typography>
+            <div className="element">
+              <FormControl className={clsx(classes.margin, classes.textField)}>
+                <InputLabel htmlFor="standard-adornment-userid">아이디</InputLabel>
+                <Controller
+                  name="userId"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      id="standard-adornment-userid"
+                      className="input-login"
+                      type="text"
+                      value={values.userId || ''}
+                      name="userId"
+                      onChange={handleChange('userId')}
+                      {...field}
+                    />
+                  )}
+                />
+              </FormControl>
             </div>
-          )}
-        </form>
-      </div>
-    </div>
+            <div className="element">
+              <FormControl className={clsx(classes.margin, classes.textField)}>
+                <InputLabel htmlFor="standard-adornment-password">비밀번호</InputLabel>
+                <Controller
+                  name="userPassword"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      id="standard-adornment-password"
+                      className="input-login"
+                      type={values.showPassword ? 'text' : 'password'}
+                      value={values.userPassword || ''}
+                      name="userPassword"
+                      onChange={handleChange('userPassword')}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}>
+                            {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                      {...field}
+                    />
+                  )}
+                />
+              </FormControl>
+            </div>
+            <div className="element">
+              <Button value="Login" className="login-btn" type="primary" htmlType="submit" block>
+                로그인
+              </Button>
+            </div>
+            <div className="element-btn">
+              <LockIcon style={{ width: 20, margin: '0 5' }}></LockIcon>
+              <Link to="/find" className="iconList">
+                <p className="label">아이디•비밀번호 찾기</p>
+              </Link>
+            </div>
+            <div className="element-btn">
+              <PersonIcon style={{ width: 20, margin: '0 5' }}></PersonIcon>
+              <Link to="/register" className="iconList">
+                <p className="label">회원가입</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Redirect to="/home" />
+      )}
+    </StyedLogin>
   )
 }
 
